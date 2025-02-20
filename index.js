56bdf1ee6afff3b1cd2e3191bcfafe90a2dae16b
Matt Carter <m@ttcarter.com>
2025-02-20 16:23:53 +1000

    refactor: Moved from CJS + lodash dep to ESM (no lodash dep)

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-import _ from 'lodash';
-
 export function keyArrange(obj, callback) {
-	let out = {};
-	Object.keys(obj)
-		.sort(callback || undefined)
-		.forEach(function(k) { out[k] = obj[k] });
-	return out;
+	return Object.fromEntries(
+		Object.entries(obj)
+			.sort(callback || undefined)
+			.map(([k, v]) => [k, v])
+	);
 };
 
 export function keyArrangeDeep(obj, callback) {
-	if (_.isArray(obj)) {
-		return _.map(obj, function(item) {
-			return keyArrangeDeep(item, callback);
-		});
-	} else if (_.isObject(obj)) {
-		let newObj = keyArrange(obj, callback);
-		_.forEach(newObj, function(val, key) {
-			newObj[key] = keyArrangeDeep(val);
-		});
-		return newObj;
-	} else {
+	if (Array.isArray(obj)) {
+		return obj.map(v => keyArrangeDeep(v, callback));
+	} else if (typeof obj == 'object') {
+		return Object.fromEntries(
+			Object.entries(
+				keyArrange(obj, callback)
+			)
+			.map(([k, v]) => [k, keyArrangeDeep(v, callback)])
+		)
+	} else { // Everything else - Assume a scalar
 		return obj;
 	}
 };
 
 export default {
-	keyArrange: keyArrange,
-	keyArrangeDeep: keyArrangeDeep,
+	keyArrange,
+	keyArrangeDeep,
 };
